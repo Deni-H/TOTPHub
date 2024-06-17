2559d74d748bb71e7b6e6a805214d138ac0f397a
Deni-H <denyhilhamsyh@gmail.com>
2024-06-17 15:58:40 +0700

    Remove minor warning

--- app/src/main/java/com/denihilhamsyah/totphub/code/presentation/CodeViewModel.kt
+++ app/src/main/java/com/denihilhamsyah/totphub/code/presentation/CodeViewModel.kt
@@ -1,6 +1,5 @@
 package com.denihilhamsyah.totphub.code.presentation
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.denihilhamsyah.totphub.code.domain.repository.TOTPRepository
--- app/src/main/java/com/denihilhamsyah/totphub/ui/theme/Theme.kt
+++ app/src/main/java/com/denihilhamsyah/totphub/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.denihilhamsyah.totphub.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
