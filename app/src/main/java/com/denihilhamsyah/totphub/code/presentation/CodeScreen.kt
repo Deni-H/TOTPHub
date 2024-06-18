75d47e86dfdde37a060cc00ffc5dd45d7103f916
Deni-H <denyhilhamsyh@gmail.com>
2024-06-18 13:43:23 +0700

    Adding DatabaseRepository Adding RealmRepositoryImpl

--- app/src/main/java/com/denihilhamsyah/totphub/code/presentation/CodeScreen.kt
+++ app/src/main/java/com/denihilhamsyah/totphub/code/presentation/CodeScreen.kt
@@ -1,19 +1,7 @@
 package com.denihilhamsyah.totphub.code.presentation
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
